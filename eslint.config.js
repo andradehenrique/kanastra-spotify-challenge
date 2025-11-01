357706167cb81e64b204857574f45bc0742ead50
Henrique Andrade <developer@henriqueandrade.dev>
2025-11-01 15:59:54 -0300

    [feat] ✨: configure tailwind, shadcn and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-plugin-prettier/recommended";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          printWidth: 100,
+          tabWidth: 2,
+          semi: true,
+          trailingComma: "es5",
+          bracketSpacing: true,
+          endOfLine: "lf",
+        },
+      ],
+    },
   },
-])
+]);
